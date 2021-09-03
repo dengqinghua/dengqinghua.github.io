2ad7dfcd766ab461fec6fde563e98ff725edc5ca
dengqinghua <dengqinghua.42@gmail.com>
2021-09-03 15:18:02 +0800

    fix gitalk

--- docs/assets/js/gitalk_init.js
+++ docs/assets/js/gitalk_init.js
@@ -3,27 +3,16 @@ var locationId = (location.pathname).split("/").pop().substring(0, 49);
 if (locationId) {
   var language = window.location.search.match("lang=en") ? "en" : "zh-CN"
 
-  if (window.location.href.match("github")) {
-    var gitalk = new Gitalk({
-      clientID: '6fb66a3a035c9e957602',
-      clientSecret: '41c3158997a8d1c1a69aa2f92b4cbece44390b30',
-      repo: 'blog-github-page-comments',
-      owner: 'dengqinghua',
-      admin: ['dengqinghua'],
-      id: locationId,
-      distractionFreeMode: true,
-      language: language
-    });
-  } else {
-    var gitalk = new Gitalk({
-      clientID: '6fb66a3a035c9e957602',
-      clientSecret: '41c3158997a8d1c1a69aa2f92b4cbece44390b30',
-      repo: 'blog_comments',
-      owner: 'dengqinghua',
-      admin: ['dengqinghua'],
-      id: locationId,
-      distractionFreeMode: true,
-      language: language
-    });
-  }
+  var gitalk = new Gitalk({
+    clientID: '6fb66a3a035c9e957602',
+    clientSecret: '399300ddd0e103846ed0bef3fd0b67938c302d1a',
+    repo: 'blog_comments',
+    owner: 'dengqinghua',
+    admin: ['dengqinghua'],
+    id: locationId,
+    distractionFreeMode: true,
+    language: language
+  });
+
+  gitalk.render('comment');
 }
